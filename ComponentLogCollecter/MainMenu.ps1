8b9011c3d2fa5b1bb2c847574204349659864bd5
Travis <travis.sanford1@gmail.com>
2020-01-21 14:37:37 -0700

    Pushed files to correct folder

--- ComponentLogCollecter/MainMenu.ps1
+++ ComponentLogCollecter/MainMenu.ps1
@@ -1,4 +1,4 @@
-#Set Domain
+
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Write-host "My directory is $dir"
@@ -6,27 +6,68 @@ Push-location $dir
 
 . .\Show-Menu.ps1
 . .\Set-UserCreds.ps1
-$domain = "NULL"
-$Section = "Domain"
-$dom = $PSScriptRoot + "\domains.txt"
-$list = get-content $dom
-$domain = Show-menu -List $list -Section $Section
+
+#Set Domain
+function Set-Domain {
+    $domain = ""
+    $Section = "Domain"
+    $dom = $PSScriptRoot + "\domains.txt"
+    $list = get-content $dom
+    $domain = Show-menu -List $list -Section $Section
+    return $domain
+}
+$domain = Set-Domain
+write-host $domain
+
+#Set-Product
+function Set-Product {
+    $product = ""
+    $Section = "Product"
+    #$pro = "C:\Users\travis.sanford\Documents\GitHub\NiCTools\ComponentLogCollecter\products.txt"
+    $pro = $PSScriptRoot + "\products.txt"
+    $list = get-content $pro
+    $product = Show-menu -List $list -Section $Section
+    return $product
+}
+$product = Set-Product
+write-host $product
 
 #Set Authentication
-$MyCreds = Set-UserCreds -domain $domain
+#$MyCred = Set-UserCreds -domain $domain
+$mycred = Set-UserCreds -domain $domain
 
 
 #Set the environment
 #Set-Environment.ps1
-$Section = "Environment"
-$environments = $PSScriptRoot + "\$domain" + "Environments.txt"
-$envlist = get-content $environments
-$enviro = Show-menu -List $envlist
-$enviro
+function Set-Environment {
+    $Section = "Environment"
+    $environments = $PSScriptRoot + "\$domain" + "Environments.txt"
+    $envlist = get-content $environments
+    $enviro = Show-menu -List $envlist
+    return $enviro
+}
+$enviro = Set-Environment
+write-host $enviro
+
+
 #Set the module (configs, servicenames, processes, logfiles)
-#Set-Module.ps1
+#Set-Component.ps1
+function Set-Components {
+    $Section = "Component"
+    $components = $PSScriptRoot + "\Components.txt"
+    #Get-Content needs a explicit delimiter set so that when we have 1 single option it will not try to go byte by byte instead of line by line. This is per Dev.
+    $complist = get-content $components
+    $component = Show-menu -List $complist
+    return $component
+}
+$component = Set-Components
+write-host $component
 
+#Set-Timeframe for query set
 
+#Set Infratools query via tag variables
+
+#Call Infratools via tags
 
 #Modify config files to debug
 #modify-configfile.ps1
@@ -36,9 +77,19 @@ $enviro
 
 #modify config files to info
 #modify-configfile.ps1
+#$deploy = "$PSScriptRoot + "\Get-ComponentLogFiles""
+
+#Upload log collector script to destination "deploy"
+#<function Deploy-Collector
+#{
+#Copy-Item -Path "$deploy" -Destination
+# }
+#Deploy-Collector
+
+
 
 #Collect logs
-#collect-logs.ps1
+#Collect-logs.ps1
 
 #optional zip logs
 #zip-files.ps1
